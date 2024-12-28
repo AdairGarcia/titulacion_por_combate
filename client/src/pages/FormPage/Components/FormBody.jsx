207a97c8c3c0cf00fb1ed2402f168ab8ded06367
AdairGarcia <agarciat1800@alumno.ipn.mx>
2024-12-28 14:35:32 -0600

    CreateQuestionModal correccion bug css

--- client/src/pages/FormPage/Components/FormBody.jsx
+++ client/src/pages/FormPage/Components/FormBody.jsx
@@ -3,17 +3,19 @@ import { IoIosAdd } from 'react-icons/io';
 import { FaDownload } from 'react-icons/fa6';
 import { MdDelete } from 'react-icons/md';
 import { Question } from './Question.jsx';
-import {CreateQuestionModal} from "./CreateQuestionModal.jsx";
+import { CreateQuestionModal } from './CreateQuestionModal.jsx';
 
 export const FormBody = ({ form }) => {
     return (
         <>
             <div className="container-fluid h-100 fondo-pantalla-principal">
-                <div className="d-flex justify-content-between">
-                    <h1 className="form-title m-5">{form.title}</h1>
-                    <div className="btn-group m-5">
+                <div className="row align-items-center justify-content-between p-3">
+                    <div className="col-auto my-5">
+                        <h1 className="form-title">{form.title}</h1>
+                    </div>
+                    <div className="col-auto d-flex flex-wrap">
                         <button
-                            className="mx-5 rounded-4 btn-agregar p-3"
+                            className="mx-2 my-2 rounded-4 btn-agregar p-3"
                             type="button"
                             data-bs-toggle="modal"
                             data-bs-target="#createQM"
@@ -21,18 +23,18 @@ export const FormBody = ({ form }) => {
                             <IoIosAdd size="35px" />
                             Agregar pregunta
                         </button>
-                        <button className="mx-5 rounded-4 btn-descargar p-3">
+                        <button className="mx-2 my-2 rounded-4 btn-descargar p-3">
                             <FaDownload size="30px" />
                             Descargar cuestionario
                         </button>
-                        <button className="mx-5 rounded-4 btn-eliminar p-2">
+                        <button className="mx-2 my-2 rounded-4 btn-eliminar p-2">
                             <MdDelete size="30px" />
                             Eliminar cuestionario
                         </button>
                     </div>
                 </div>
 
-                <div>
+                <div className={"m-3"}>
                     <Question
                         question={{
                             questionText: '¿Cuál es la capital de México?',
@@ -59,7 +61,7 @@ export const FormBody = ({ form }) => {
                     />
                 </div>
             </div>
-            <CreateQuestionModal/>
+            <CreateQuestionModal />
         </>
     );
 };
